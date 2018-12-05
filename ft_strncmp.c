1a97161be08bc03d18bfe4d061aaab2f02f469cc
seefh <seefh@jador.fr>
2018-12-05 17:11:40 +0100

    Add files via upload

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: shamidan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 15:35:34 by shamidan          #+#    #+#             */
-/*   Updated: 2018/12/05 15:57:41 by shamidan         ###   ########.fr       */
+/*   Updated: 2018/12/05 17:10:10 by shamidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int i;
+	size_t	i;
 
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n - 1)
