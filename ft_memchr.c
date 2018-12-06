b0448df0e80ab46da4ccc8e417d80d44112c2438
seefh <seefh@jador.fr>
2018-12-06 18:00:58 +0100

    Add files via upload

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamidan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/06 15:55:44 by shamidan          #+#    #+#             */
+/*   Updated: 2018/12/06 17:57:48 by shamidan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+void	*ft_memchr(const void *s, int c, size_t n)
+{
+	char			*src;
+	unsigned char	charac;
+	size_t			i;
+
+	src = (char *)s;
+	charac = (unsigned char)c;
+	i = 0;
+	while (n--)
+	{
+		if (src[i] == charac)
+			return (&src[i]);
+		i++;
+	}
+	return (NULL);
+}
