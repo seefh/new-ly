8c766a4c15e009516f1c575ee3a792e0801ad8f9
seefh <seefh@jador.fr>
2018-12-11 11:55:01 +0100

    Add files via upload

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,9 +6,26 @@
 /*   By: shamidan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/09 19:18:27 by shamidan          #+#    #+#             */
-/*   Updated: 2018/12/09 19:18:30 by shamidan         ###   ########.fr       */
+/*   Updated: 2018/12/11 11:44:54 by shamidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
-char	*strdup(const char *s1);
+char	*ft_strdup(const char *str1)
+{
+	char	*memory;
+	char	*s1;
+	int		i;
+
+	s1 = (char *)str1;
+	i = 0;
+	memory = (char*)malloc(sizeof(*s1) * (ft_strlen(s1) + 1));
+	while (s1[i])
+	{
+		memory[i] = s1[i];
+		i++;
+	}
+	memory[i] = '\0';
+	return (memory);
+}
