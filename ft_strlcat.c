54b09d346ba0b59f8eb9702be60e6dde3e7fad5d
seefh <seefh@jador.fr>
2018-12-12 16:15:04 +0100

    Add files via upload

---
+++ ft_strlcat.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamidan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/28 16:28:13 by shamidan          #+#    #+#             */
+/*   Updated: 2018/12/11 17:58:25 by shamidan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+	size_t	dstl;
+	size_t	srcl;
+
+	i = 0;
+	dstl = ft_strlen(dst);
+	srcl = ft_strlen(src);
+	if (size <= dstl)
+		return (srcl + size);
+	while ((dst[i] != '\0') && i < (size - 1))
+		i++;
+	while (*src && i < (size - 1))
+	{
+		dst[i] = *src;
+		i++;
+		src++;
+	}
+	dst[i] = '\0';
+	return (dstl + srcl);
+}
