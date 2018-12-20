52e2ef38377bfa36b70a1875abb4ecb74aca8d47
seefh <seefh@jador.fr>
2018-12-20 14:11:05 +0100

    Add files via upload

---
+++ ft_strmapi.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmap.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamidan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/20 12:53:06 by shamidan          #+#    #+#             */
+/*   Updated: 2018/12/20 14:09:55 by shamidan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*s1;
+	char			*s2;
+	unsigned int	i;
+
+	i = 0;
+	s1 = (char *)s;
+	s2 = malloc(sizeof(s1));
+	while (s1[i] != '\0')
+	{
+		s2[i] = f(i, s1[i]);
+		i++;
+	}
+	return (s2);
+}
