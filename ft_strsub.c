5a6e11cd72eb630918a376d5826e4e4ac1435eb8
seefh <seefh@jador.fr>
2018-12-20 16:27:53 +0100

    Add files via upload

---
+++ ft_strsub.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: shamidan <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/12/20 14:50:45 by shamidan          #+#    #+#             */
+/*   Updated: 2018/12/20 16:26:37 by shamidan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char	*str;
+	char	*dest;
+	size_t	i;
+	size_t	j;
+
+	str = (char *)s;
+	dest = malloc(len + start);
+	i = 0;
+	j = start;
+	while (j < len + start)
+	{
+		dest[i] = str[j];
+		i++;
+		j++;
+	}
+	return (dest);
+}
