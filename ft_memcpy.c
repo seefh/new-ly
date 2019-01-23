6fc0d8d7f9fb1b29857cd4ca799523d6dcb61ca8
seefh <seefh@jador.fr>
2019-01-23 13:32:54 +0100

    Add files via upload

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: shamidan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 17:27:39 by shamidan          #+#    #+#             */
-/*   Updated: 2018/12/07 12:00:33 by shamidan         ###   ########.fr       */
+/*   Updated: 2019/01/23 13:31:28 by shamidan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,10 @@
 
 void	*ft_memcpy(void *restrict dst, const void *restrict src, size_t n)
 {
-	char	*source;
-	char	*destination;
-	size_t	i;
+	size_t i;
 
-	i = 0;
-	destination = (char *)dst;
-	source = (char *)src;
-	while (i < n)
-	{
-		destination[i] = source[i];
-		i++;
-	}
-	return (destination);
+	i = -1;
+	while (++i < n)
+		((char *)dst)[i] = ((char *)src)[i];
+	return (dst);
 }
